063ad8a12ca282622065dd1ee7ba900f765f0bd6
jesse python <jessepythondev@gmail.com>
2025-08-11 06:42:37 +0000

    https://images.unsplash.com/photo-1713526721814-412688ede906?q=80&w=687&

--- src/components/sections/TeamSection.tsx
+++ src/components/sections/TeamSection.tsx
@@ -23,7 +23,7 @@ const teamMembers = [
   {
     name: 'Maria Garcia',
     role: 'Head of UX/UI Design',
-    avatarUrl: 'https://placehold.co/400x400.png',
+    avatarUrl: 'https://images.unsplash.com/photo-1713526721814-412688ede906?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     aiHint: 'woman designer',
   },
   {
